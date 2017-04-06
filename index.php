09a4cca3dcb06d4aedff87baf13a902a28a2cce1
Henrik <henriknorinder_88@hotmail.com>
2017-04-06 13:35:10 +0200

    nu har jag ändrat igen

--- index.php
+++ index.php
@@ -3,6 +3,21 @@ require 'vendor/autoload.php';
 
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
+
 $log = new Logger('Laboration 1');
 $log->pushHandler(new StreamHandler('greetings.log', Logger::INFO));
-$log->info("Hello ".$argv[1]. " whata sick name!");
+
+$name = $_GET['name'];
+$log->info($name);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Testing, testing</title>
+</head>
+<body>
+<?php
+echo "Hello, " . $name;
+?>
+</body>
+</html>
